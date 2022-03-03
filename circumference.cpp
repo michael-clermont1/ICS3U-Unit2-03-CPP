17362a7b0b1d5d7009a4f24a00896f2186409b77
Michael Clermont <michael.clermont@mths.ca>
2022-03-03 19:09:42 +0000

    added circumference program

---
+++ circumference.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 Michael Clermont All rights reserved
+//
+// Created by: Michael Clermont
+// Created on: Feb 2022
+// This program calculates the circumference of a circle
+//    with radius inputted by the user and tau
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    // this function calculates the area and perimeter inputted by user
+    const double TAU = 6.28;
+    double circumference;
+    int radius;
+
+    // input
+    std::cout << "Enter radius of the circle (mm): ";
+    std::cin >> radius;
+
+    // process
+    circumference = radius*TAU;
+
+    // output
+    std::cout << "" << std:: endl;
+    std::cout << "Circumference is "<< circumference << "mm." << std::endl;
+    std::cout<< "\nDone." << std::endl;
+}
